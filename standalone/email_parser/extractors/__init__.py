156ca8f55df0fc24419556230f07f3d8715302ec
cd1zz <craigfreyman@gmail.com>
2025-07-07 07:52:27 -0600

    Add domain extraction support

--- standalone/email_parser/extractors/__init__.py
+++ standalone/email_parser/extractors/__init__.py
@@ -1,9 +1,11 @@
 from .url_extractor import UrlExtractor, UrlExtractionResult
 from .url_processor import UrlProcessor, ProcessedUrl
 from .url_analyzer import UrlAnalyzer, UrlAnalysisResult
+from .domain_extractor import DomainExtractor, DomainExtractionResult
 
 __all__ = [
     'UrlExtractor', 'UrlExtractionResult',
-    'UrlProcessor', 'ProcessedUrl', 
-    'UrlAnalyzer', 'UrlAnalysisResult'
+    'UrlProcessor', 'ProcessedUrl',
+    'UrlAnalyzer', 'UrlAnalysisResult',
+    'DomainExtractor', 'DomainExtractionResult'
 ]
